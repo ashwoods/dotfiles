738767a6ad23dda1428a736818ab54b3b35ced3f
Ashley Camba Garrido <ashwoods@gmail.com>
2022-03-10 20:56:18 +0000

    Fix starship init

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    source ("/usr/local/bin/starship" init fish --print-full-init | psub)
-    #status is-login; and pyenv init --path | source
+    starship init fish | source
     pyenv init - | source
 end
 
